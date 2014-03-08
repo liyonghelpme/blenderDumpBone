4ec61bc00e449bddb59569937b2c90fc83f1d768
liyonghelpme <liyonghelpme@gmail.com>
2014-03-08 11:28:54 +0800

    dump data

---
+++ boneTest.py
@@ -0,0 +1,85 @@
+import bpy, math
+from mathutils import Vector, Matrix
+import mathutils
+
+def dumpVertData():
+    obj = bpy.data.objects['Cube']
+    vert = obj.data.vertices
+    arm = bpy.data.objects['Armature']
+    
+    vert_groups = obj.vertex_groups.values()
+    group_tot = len(obj.vertex_groups)
+     
+    if not group_tot:
+        weight_ls = [[] for i in range(len(vert))]
+    else:
+        weight_ls = [[0.0]*group_tot for i in range(len(vert))]
+    
+    for i, v in enumerate(vert):
+        for g in v.groups:
+            index = g.group
+            if index < group_tot:
+                weight_ls[i][index] = g.weight
+    
+    
+    f = open('vert.dat', 'w')
+    f.write("%d\n" % len(vert))
+    for v in vert:
+        f.write('%f %f %f\n'% (v.co.x, v.co.y, v.co.z))
+    f.write("%d\n"% group_tot)
+    for i, w in enumerate(weight_ls):
+        for k in w:
+            f.write("%f "%(k))
+        f.write('\n')
+    f.close()
+    
+    #groups---> group0  group 1
+    #vert ---> groupId ---> weight
+    
+    '''
+    count = 0
+    s = ''
+    for v in vert:
+        for g in v.groups:
+            s += "%d %d %f\n" % (v.index, g.group, g.weight)
+            count = count+1
+    f.write('%d\n'% count)
+    f.write(s)
+    f.close() 
+    '''
+    
+def dumpFaceData():
+    fi = open('face.dat', 'w')
+    mesh = bpy.data.meshes['Cube']
+    fi.write('%d\n' % (len(mesh.faces)))
+    for f in mesh.faces:
+        s = ''
+        s += '%d\n' % (len(f.vertices))
+        for i in f.vertices:
+            s += '%d ' % (i) 
+        s += '\n'
+        fi.write(s)
+    fi.close()
+      
+    
+def dumpBoneData():
+    bv = bpy.data.armatures.get('Armature').bones.values()
+    f = open('bone.dat', 'w')
+    f.write('%d\n'%(len(bv)))
+    for k in bv:
+        q = k.matrix.to_quaternion()
+        f.write('%f %f %f %f\n' % (q.x, q.y, q.z, q.w))
+        f.write('%f\n'% (k.length))
+        f.write('%f %f %f\n'%( k.head.x, k.head.y, k.head.z))
+        if k.parent == None:
+            f.write('-1\n')
+        else:
+            f.write('%d\n' % (bv.index(k.parent)))
+    f.close()        
+
+
+if __name__ == '__main__':
+    dumpVertData()
+    dumpFaceData()
+    dumpBoneData()    
+        
